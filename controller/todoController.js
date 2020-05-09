4837009ebcc9da074aec3f2eafe4baa8e189e316
handsomeke <ww1119084200qq.com@qq.com>
2020-05-09 17:40:52 +0800

    删除

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,13 +1,37 @@
+var bodyParser = require('body-parser')
+
+var urlencodeParser = bodyParser.urlencoded({
+    extended: false
+});
+
+var data = [{
+        item: "欢迎大家来到hello world!"
+    },
+    {
+        item: "欢迎大家来到hello world!123123"
+    },
+    {
+        item: "欢迎大家来到hello world!1234324325"
+    }
+]
+
+
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render('todo')
+        res.render('todo', {
+            todos: data
+        })
     })
 
-    app.post('/todo', function (req, res) {
-
+    app.post('/todo', urlencodeParser, function (req, res) {
+        data.push(req.body)
     })
 
-    app.delete('/todo', function (req, res) {
+    app.delete('/todo/:item', function (req, res) {
+        data = data.filter(function (todo) {
+            return req.params.item !== todo.item;
+        })
 
+        res.json(data);
     })
 }
